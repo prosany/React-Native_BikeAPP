1ff18716a8a70378eaae1279ebbea8b3eaa6e094
habiba2012 <habibaakhter2012@gmail.com>
2021-06-03 08:33:33 +0200

    update

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -4,8 +4,6 @@ import styles from './styles';
 
 const StyledButton = (props) => {
     const { type, content, onPress } = props;
-
-
     const backgroundColor = type => 'primary' ? '#171A20CC' : '#FFFFFFA6';
     const textColor = type => 'primary' ? 'white' : 'black';
     return (
