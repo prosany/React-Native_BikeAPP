4f1f6775e6ad43f4cef3d0253fcd57500c94784c
Mahabub Sany <mahabubsany2@gmail.com>
2021-06-03 13:10:10 +0600

    [#2 Change]

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import BikesList from './components/BikesList';
 import Header from './components/Header';
 
